d4fb27bf69abad5ee17512d0784395072c42c104
Jayson-Marinho <126027989+Jayson-Marinho@users.noreply.github.com>
2023-03-14 19:57:12 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+function home(){
+    return <div class="conteiner">
+            <div class="resultado">
+                <span>0</span>
+            </div>
+    
+            <div class="botoes">
+                <button class="item-limpar">AC</button>
+                <button class="item-negativo">+/-</button>
+                <button class="item-porcento">%</button>
+                <button class="item-dividir">/</button>
+                <button class="item-numeros">7</button>
+                <button class="item-numeros">8</button>
+                <button class="item-numeros">9</button>
+                <button class="item-multiplicar">X</button>
+                <button class="item-numeros">4</button>
+                <button class="item-numeros">5</button>
+                <button class="item-numeros">6</button>
+                <button class="item-subtrair">-</button>
+                <button class="item-numeros">1</button>
+                <button class="item-numeros">2</button>
+                <button class="item-numeros">3</button>
+                <button class="item-somar">+</button>
+                <button class="item-numeros">0</button>
+                <button class="item-virgula">,</button>
+                <button class="item-resultado">=</button>
+            </div>
+        </div>
+}
+
+export default home
